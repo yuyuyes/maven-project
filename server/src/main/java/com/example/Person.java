c43af5b4d5291897a50e3271d26895b9b43e0edc
Jenny Liang <jennyliang@liscoglobal.com>
2020-07-07 11:29:31 +0800

    fix checkstyle warning

--- server/src/main/java/com/example/Person.java
+++ server/src/main/java/com/example/Person.java
@@ -12,8 +12,7 @@ public class Person {
 
   }
 
-  //TODO: Add javadoc comment
-  public String person(String someone) {
+  public String person(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
